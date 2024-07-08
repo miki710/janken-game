af2cc71c459e87bf55035218179fbece9876487a
miki710 <brown.opium.sugar@gmail.com>
2024-07-09 00:36:47 +0900

    useContextをインポート

--- src/RoomSelectionPage.js
+++ src/RoomSelectionPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { UserContext } from './UserContext.js'; // UserContextをインポート
 
